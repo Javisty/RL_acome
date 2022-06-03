bfc93148f48a21b6e7e588d084ca0c8c059abf0d
Javisty <aymec.78620@gmail.com>
2022-06-03 11:01:36 +0200

    Add tests for SUCRL and QL_options classes

--- tests/test_learners.py
+++ tests/test_learners.py
@@ -1,16 +1,39 @@
 import numpy as np
 
-from rl_acome.envs.instances.rooms import one_room
-from rl_acome.learners.UCRL2 import UCRL2
+from rl_acome.envs.instances import rooms
+from rl_acome.learners import UCRL2, SUCRL, QL_options
 
 
-grid_world = one_room()
+one_room = rooms.one_room()
+four_rooms, _, options = rooms.four_rooms_with_options()
+
 
 class TestUCRL2:
     def test_init(self):
-        agent = UCRL2(grid_world)
+        agent = UCRL2.UCRL2(one_room)
 
     def test_fit(self):
-        agent = UCRL2(grid_world)
+        agent = UCRL2.UCRL2(one_room)
+
+        agent.fit(50)
+
+
+class TestSUCRL:
+    def test_init(self):
+        agent = SUCRL.SUCRL(four_rooms, options, 20, 1, 0, 1, 0, 17)
+
+    def test_fit(self):
+        agent = SUCRL.SUCRL(four_rooms, options, 20, 1, 0, 1, 0, 17)
+
+        agent.fit(50)
+
+
+
+class TestQL_options:
+    def test_init(self):
+        agent = QL_options.QL_options(four_rooms, options)
+
+    def test_fit(self):
+        agent = QL_options.QL_options(four_rooms, options)
 
         agent.fit(50)

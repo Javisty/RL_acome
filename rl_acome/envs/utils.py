8787190f395bbdbdca2015c518152de850b3c82e
Javisty <aymec.78620@gmail.com>
2022-06-16 15:15:17 +0200

    Estimate gain

--- rl_acome/envs/utils.py
+++ rl_acome/envs/utils.py
@@ -68,3 +68,28 @@ def plot_gridworld(env, colormap='Oranges', save_path=None):
         plt.imsave(save_path, img)
 
     return plt.imshow(img)
+
+
+def greedy_policy(q):
+    """
+    Return the greedy policy for a Q-value function.
+
+    The greedy policy selects the action maximising the value. If there are
+    multiple candidates, choose randomly.
+
+    Parameters:
+    -----------
+    q : (S, A) numpy.array
+        Q-value vector.
+
+    Output:
+    -------
+    S numpy.array, giving the action chosen for each state.
+    """
+    S = q.shape[0]
+    policy = np.zeros(S, dtype=int)
+
+    for s in range(S):
+        policy[s] = np.random.choice(np.where(q[s, :] == np.max(q[s, :]))[0])
+
+    return policy
--- rl_acome/envs/wrappers.py
+++ rl_acome/envs/wrappers.py
@@ -3,6 +3,8 @@
 import numpy as np
 from rlberry.envs import Wrapper
 
+from .utils import greedy_policy
+
 
 class EvaluatingEnv(Wrapper):
     """
@@ -25,10 +27,12 @@ class EvaluatingEnv(Wrapper):
         provided, opti_V is computed (and overwritten) from opti_Q.
     opti_reward : S numpy.array or None, default None
         Maximum expected reward at each state, if provided.
+    opti_gain : float or None, default None
+        Optimal gain, if provided.
     """
 
     def __init__(self, env, opti_policy=None, opti_Q=None,
-                 opti_V=None, opti_reward=None):
+                 opti_V=None, opti_reward=None, opti_gain=None):
         Wrapper.__init__(self, env)
 
         self.opti_pi = opti_policy
@@ -64,31 +68,136 @@ class EvaluatingEnv(Wrapper):
 
     def estimate_optimal_reward(self, n_iter=20):
         """
-        Estimate the expected reward for optimal policy.
+        Estimate the expected reward in each state for optimal policy.
 
         For each state-action pair, average reward over n_iter iterations.
         Overwrite self.opti_r. If an optimal policy wasn't provided, use the
         greedy policy from the optimal Q-value function. If not provided
         neither, raise an error.
         """
-        if self.opti_pi:
+        if self.opti_pi is not None:
             pi = self.opti_pi
         else:
             assert self.opti_Q, "No optimal policy nor Q function provided!"
-            pi = np.zeros((self.S, self.A))
-            pi[range(self.S), np.argmax(self.opti_Q, axis=-1)] = 1.
+            pi = greedy_policy(self.opti_Q)
 
         env = self.env
+        self.opti_r = np.zeros(self.S)
         for s in range(self.S):
-            a = np.random.choice(self.A, p=pi[s, :])
             R = 0
             for _ in range(n_iter):
                 env.state = s
+                a = np.random.choice(self.A, p=pi[s, :])
                 _, r, _, _ = env.step(a)
                 R += r
             self.opti_r[s] = R / n_iter
 
         return self.opti_r
+
+    def estimate_state_gain(self, policy, s0, traj_length=10000, burn_in=1000):
+        """
+        Estimate the gain of policy, starting from state s0.
+
+        Run policy over traj_length steps, and return the average reward
+        collected after a burn-in period.
+
+        Parameters:
+        -----------
+        policy : (S, A) numpy.array
+            Policy to use.
+        s0 : 0 <= integer < self.S
+            Initial state.
+        traj_length : integer > 0, default 10000.
+            Number of steps to execute under policy.
+        burn_in : 0 <= integer < traj_length, default 1000
+            Number of burn-in steps to make, before collecting rewards.
+
+        Output:
+        -------
+        gain : float
+            Estimated gain of policy starting from s0.
+        """
+        assert burn_in < traj_length, "Burn-in length must be less than total!"
+
+        env = self.env
+        env.state, s = s0, s0
+
+        for _ in range(burn_in):
+            a = np.random.choice(self.A, p=policy[s, :])
+            s, _, _, _ = env.step(a)
+
+        R = 0  # start collecting rewards
+        t = traj_length - burn_in
+        for _ in range(t):
+            a = np.random.choice(self.A, p=policy[s, :])
+            s, r, _, _ = env.step(a)
+            R += r
+
+        return R / t
+
+    def estimate_gain(self, policy, traj_length=10000, burn_in=1000):
+        """
+        Estimate the gain vector of policy.
+
+        Run policy over traj_length steps, and return the average reward
+        collected after a burn-in period, for all starting state.
+
+        See self.estimate_state_gain
+
+        Parameters:
+        -----------
+        policy : (S, A) numpy.array
+            Policy to use.
+        traj_length : integer > 0, default 10000.
+            Number of steps to execute under policy.
+        burn_in : 0 <= integer < traj_length, default 1000
+            Number of burn-in steps to make, before collecting rewards.
+
+        Output:
+        -------
+        gain : S numpy.array
+            Estimated gain vector of policy over states.
+        """
+        state_gain = self.estimate_state_gain
+        return np.array([state_gain(policy, s, traj_length, burn_in)
+                         for s in range(self.S)])
+
+    def estimate_optimal_gain(self, traj_length=10000, burn_in=1000):
+        """
+        Estimate the optimal gain.
+
+        Run policy over traj_length steps, and return the average reward
+        collected after a burn-in period.
+        !!! It is assumed that the environment is weakly communicating, and
+        hence the optimal gain is independent of the starting state !!!
+
+        The environment initial distribution is then used.
+        Use self.opti_pi if provided, else greedy_policy(self.opti_Q).
+        Overwrite self.opti_gain.
+
+        See self.estimate_gain
+
+        Parameters:
+        -----------
+        traj_length : integer > 0, default 10000.
+            Number of steps to execute under policy.
+        burn_in : 0 <= integer < traj_length, default 1000
+            Number of burn-in steps to make, before collecting rewards.
+
+        Output:
+        -------
+        gain : float
+            Estimated optimal gain.
+        """
+        if self.opti_pi is not None:
+            pi = self.opti_pi
+        else:
+            assert self.opti_Q, "No available optimal policy!"
+            pi = greedy_policy(self.opti_Q)
+
+        s0 = self.env.reset()
+        self.opti_gain = self.estimate_state_gain(pi, s0, traj_length, burn_in)
+        return self.opti_gain
 
     def value_error(self, value):
         """Return error of value w.r.t. optimal value."""
